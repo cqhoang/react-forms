9cd240d9f08361eb9922916b704e5c5c23024251
Christina <christinah.0821@gmail.com>
2024-04-21 12:33:50 -0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   const [count, setCount] = useState(0);
